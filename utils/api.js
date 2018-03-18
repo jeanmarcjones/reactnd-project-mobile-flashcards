548a8634515dfb7c4d9b8b529d3e3dd6e8e64e69
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-03-18 11:54:14 +0000

    feat: created AsyncStorage helper functions

---
+++ utils/api.js
@@ -0,0 +1,41 @@
+import { AsyncStorage } from 'react-native'
+
+export const DECK_STORAGE_KEY = 'FlashCards:deck'
+
+// Initial decks
+export function receiveDecks(decks) {
+  return AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(decks))
+}
+
+// return all of the decks along with their titles, questions, and answers.
+export function getDecks() {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((res) => {
+      return JSON.parse(res)
+    })
+}
+
+// take in a single id argument and return the deck associated with that id.
+export function getDeck(deck) {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((res) => {
+      const data = JSON.parse(res)
+      return data[deck]
+    })
+}
+
+// take in a single title argument and add it to the decks.
+export function saveDeckTitle(deck) {
+  return AsyncStorage.setItem(DECK_STORAGE_KEY, deck)
+}
+
+// take in two arguments, title and card, and will add the card to the list of questions for the deck with the
+// associated title.
+export function addCardToDeck(title, card) {
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((results) => {
+      const data = JSON.parse(results)
+      data[title].questions = [...data[title].questions, card]
+      AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+    })
+}
