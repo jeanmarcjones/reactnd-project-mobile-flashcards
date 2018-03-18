6636213c6487ff8b9dd4d548861d19691206892b
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-03-18 11:43:08 +0000

    feat: created color constants

---
+++ utils/colors.js
@@ -0,0 +1,11 @@
+// utils/colors.js
+
+export const white = '#fff'
+export const black = '#000'
+export const grey = '#555'
+export const lightGray = '#7b7b7b'
+export const purple = '#4a148c'
+export const lightPurple = '#5C2B97'
+export const green = '#148c4a'
+export const lightGreen = '#2b975c'
+
