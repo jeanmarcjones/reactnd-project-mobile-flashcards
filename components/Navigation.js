b3847b8a2ffe43caa7f7bf640d13e1822823743c
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-03-19 19:08:11 +0000

    refactor: created navigation component

---
+++ components/Navigation.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Platform, View, StatusBar } from 'react-native'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { connect } from 'react-redux'
+import { Constants } from 'expo'
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons'
+import { white, purple, lightPurple } from '../utils/colors'
+import { fetchDecks, receiveDecks } from '../actions'
+import Decks from '../components/Decks'
+import Deck from '../components/Deck'
+import NewDeck from '../components/NewDeck'
+import AddCard from '../components/AddCard'
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor} />
+    },
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <Entypo name='add-to-list' size={30} color={tintColor} />
+    },
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+  },
+})
+
+const MainNavigation = StackNavigator({
+  Home: {
+    screen: Tabs,
+  },
+  Deck: {
+    screen: Deck,
+  },
+  AddCard: {
+    screen: AddCard,
+  },
+}, {
+  headerMode: 'screen',
+  navigationOptions: {
+    headerBackTitle: null,
+    headerTintColor: white,
+    headerStyle: {
+      backgroundColor: lightPurple,
+    }
+  },
+})
+
+class Navigation extends Component {
+  componentWillMount() {
+    const { fetchDecks } = this.props
+    fetchDecks()
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+        <MainNavigation />
+      </View>
+    );
+  }
+}
+
+const mapDispatchToProp = {
+  fetchDecks,
+}
+
+export default connect(
+  null,
+  mapDispatchToProp,
+)(Navigation)
