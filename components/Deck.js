d13c3e892f161f09eec467dc133850b3f2bd96dd
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-03-22 19:08:39 +0000

    fix: show deck title in header

--- components/Deck.js
+++ components/Deck.js
@@ -1,36 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { deckIndex } from '../utils/helpers'
 import { black, grey, lightGray, white } from '../utils/colors'
 import AddCard from "./AddCard";
 
-function Deck({ navigation }) {
-  const navigationOptions = ({ navigation }) => ({
+class Deck extends Component {
+  static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.deck.title
   })
-  const { title, questions } = navigation.state.params.deck
+  render() {
+    const { navigate } = this.props.navigation
+    const { title, questions } = this.props.navigation.state.params.deck
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.cards}>{questions.length} Cards</Text>
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.cards}>{questions.length} Cards</Text>
+        </View>
+        <View style={styles.footer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigate(
+              'AddCard',
+              { index: deckIndex(title) }
+            )}>
+            <Text style={styles.buttonText}>Add Card</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, { backgroundColor: '#000' }]}>
+            <Text style={[styles.buttonText, { color: white }]}>Start Quiz</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.footer}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate(
-            'AddCard',
-            { index: deckIndex(title) }
-          )}>
-          <Text style={styles.buttonText}>Add Card</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, { backgroundColor: '#000' }]}>
-          <Text style={[styles.buttonText, { color: white }]}>Start Quiz</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
